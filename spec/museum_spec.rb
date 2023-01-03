b7d6d19fec1ccbb95f8d1b91e45e18ee1376c2e5
James Taylor <jrtaylor0614@gmail.com>
2023-01-03 10:56:00 -0700

    :feat: started Musem class

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -1,3 +1,21 @@
+require 'rspec'
 require './lib/museum'
 require './lib/patron'
 require './lib/exhibit'
+
+
+RSpec.describe Museum do 
+    let(:dmns) do 
+        Museum.new("Denver Museum of Nature and Science")
+    end 
+
+    describe "#initialize" do 
+        it "exhists" do 
+            expect(dmns.name).to eq("Denver Museum of Nature and Science")
+        end
+
+        it "has attributes" do
+            expect(dmns.exhibits).to eq([])
+        end
+    end
+end
--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -3,26 +3,26 @@ require './lib/exhibit'
 require './lib/patron'
 
 RSpec.describe Patron do
-    let(:patron_1) do 
-         Patron.new("Bob", 20)
-      end
-      
-      describe '#initialize' do 
-          it "exhists" do 
-              expect(patron_1).to be_an_instance_of(Patron)
-          end
+   let(:patron_1) do 
+        Patron.new("Bob", 20)
+    end
+
+    describe '#initialize' do 
+        it "exhists" do 
+            expect(patron_1).to be_an_instance_of(Patron)
+        end
   
-          it "has attributes" do
-              expect(patron_1.name).to eq("Bob")
-              expect(patron_1.age).to eq(20)
-              expect(patron_1.spending_money).to eq(20)
-              expect(patron_1.interests).to eq([])
-          end
+        it "has attributes" do
+            expect(patron_1.name).to eq("Bob")
+            expect(patron_1.age).to eq(20)
+            expect(patron_1.spending_money).to eq(20)
+            expect(patron_1.interests).to eq([])
+        end
           
-          it "Can have intrests" do 
-            patron_1.add_interest("Dead Sea Scrolls")
-            patron_1.add_interest("Gems and Minerals")
-            expect(patron_1.interests).to eq(["Dead Sea Scrolls", "Gems and Minerals"])
-          end
+        it "Can have intrests" do 
+        patron_1.add_interest("Dead Sea Scrolls")
+        patron_1.add_interest("Gems and Minerals")
+        expect(patron_1.interests).to eq(["Dead Sea Scrolls", "Gems and Minerals"])
+        end
       end
   end 
